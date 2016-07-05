29b89c66fafc22ed2f84c861710572aad33df863
liu0fanyi <liu0fanyi@gmail.com>
2016-07-05 18:37:23 +0800

    ok we stop detectMultiScale now

--- TestOnePicture_py3.py
+++ TestOnePicture_py3.py
@@ -3,23 +3,25 @@
 import cv2
 import numpy as np
 import csv as csv
+import pandas as pd
+import math
 
-#Load 'test.csv' file
-test_file = open('test.csv','rb')
-test_file_object =csv.reader(test_file)
-header_test = test_file_object.next()
+# Load 'test.csv' file
+test_file = open('./training.csv')
+test_file_object = csv.reader(test_file)
+header = next(test_file_object)
 
-#create a test_data to store array
 test_data = []
 for row in test_file_object:
-	test_data.append(row[1].split(" "))
+    test_data.append(row[30].split(" "))
 
 #Just show one Picture for test
 show_window = np.array(test_data[0])
 show_window.shape = 96,96
+np.dtype(np.uint8)
 cv2.namedWindow("Picture")
-cv2.imshow("Picture",show_window.astype(np.float)/255)
-cv2.imwrite("D:\\Picture.png", show_window.astype(np.uint8))  #-->not work
+cv2.imshow('Picture',show_window.astype(np.float)/255)
+cv2.imwrite('Picture.jpg', show_window.astype(np.uint8))  #-->not work
 cv2.waitKey(0)
 
 #HAAR classfier
@@ -29,7 +31,8 @@ cv2.equalizeHist(image)
 divisor=8
 h, w = (96,96)
 minSize=(w/divisor, h/divisor)
-faceRects = classfier.detectMultiScale(image, 1.2, 2, cv2.CASCADE_SCALE_IMAGE,minSize)
+print(type(minSize))
+faceRects = classfier.detectMultiScale(image, 1.2, 2, cv2.CASCADE_SCALE_IMAGE,math.floor(minSize))
 #draw the rectangle of the face
 for faceRect in faceRects:
 	x, y, w, h = faceRect
